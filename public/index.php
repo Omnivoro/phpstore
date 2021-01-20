874d3826762dcdd869c01300e0973c40cd27f4d3
Omnivoro <omnivoro@gmail.com>
2021-01-20 21:40:32 +0000

    Required autoload.php and created Database.php
    The Database.php file is in the core\classes namespace. In order to get
    access to namespaces the autoload.php file was required once.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 
+use core\classes\Database;
+
 // abrir a sessao
 session_start();
 
 // carregar o config
 require_once('../config.php');
+
+// carrega todas as classes do projeto
+require_once('../vendor/autoload.php');
+
+$a = new Database();
