cf2d05bf3edba17155dc87cf526fc5d355ed1e93
Omnivoro <omnivoro@gmail.com>
2021-01-23 17:21:30 +0000

    Editing the index.php file to receive fetched data
    The index.php file shows an array on the screen.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use core\classes\Database;
+
 // abrir a sessao
 session_start();
 
@@ -8,3 +10,10 @@ require_once('../config.php');
 
 // carrega todas as classes do projeto
 require_once('../vendor/autoload.php');
+
+$bd = New Database();
+$clientes = $bd->select("SELECT * FROM clientes");
+echo '<pre>';
+print_r($clientes);
+echo '</pre>';
+
