578671d1ffbc50dc9d7db1a7e2e12f2566a4a5ab
Omnivoro <omnivoro@gmail.com>
2021-01-31 11:46:48 +0000

    Creating the static method Layout()
    The Layout () method includes the sequence of php file that will be
    where this function be called. It's a static method because the class
    Funtions won't be instantiated in ordet to use this method.

--- core/classes/Functions.php
+++ core/classes/Functions.php
@@ -2,9 +2,23 @@
 
 namespace core\classes;
 
+use Exception;
+
 class Functions{
-	
-	public function teste(){
-        echo 'olá';
+
+    // ===========================================================
+    public static function Layout($estruturas){
+
+        // verifica se estruturas é um array
+        if(!is_array($estruturas)){
+            throw new Exception("Coleção de estruturas inválida");
+        }
+
+        // variáveis
+        
+        // apresentar as views da aplicação
+        foreach($estruturas as $estrutura){
+            include("../core/views/$estrutura.php");
+        }
     }
 }
