17cf333cf53e03fd18a9289c16c6115cddf6abf6
Omnivoro <omnivoro@gmail.com>
2021-02-03 19:35:39 +0000

    Linking the bootstrap and app css files
    This files are linked to the html_header.php file.

--- core/views/pagina_inicial.php
+++ core/views/pagina_inicial.php
@@ -1,7 +1,6 @@
-<h1><?= $titulo ?></h1>
-
-<ul>
-<?php foreach($clientes as $cliente):?>
-    <li><?= $cliente ?></li>
-<?php endforeach;?>
-</ul>
+<h1>Titulo</h1>
+<h2>Titulo</h2>
+<h3>Titulo</h3>
+<h4>Titulo</h4>
+<h5>Titulo</h5>
+<h6>Titulo</h6>
