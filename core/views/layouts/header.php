f41e799aa910747250d2713896d73a3b01180661
Omnivoro <omnivoro@gmail.com>
2021-02-06 13:12:04 +0000

    Adding dynamic links to the header.php file
    Depending on a client has an active session or not, the header.php file
    shows some links or others one using a conditional struture.

--- core/views/layouts/header.php
+++ core/views/layouts/header.php
@@ -1,3 +1,8 @@
+<?php 
+	use core\classes\Store;
+	//$_SESSION['cliente'] = 1;
+?>
+
 <div class="container-fluid navegacao">
 	<div class="row">
 		<div class="col-6 text-start p-3">
@@ -8,6 +13,14 @@
 		<div class="col-6 text-end p-3">		
 			<a href="?a=inicio">Início</a>
 			<a href="?a=loja">Loja</a>
+			<!--Verifca se existe cliente na sessão -->
+			<?php if(Store::clienteLogado()):?>
+				<a href="">A minha conta</a>
+				<a href="">Logout</a>
+			<?php else:?>
+				<a href="">Login</a>
+				<a href="">Criar conta</a>
+			<?php endif;?>
 			<a href="?a=carrinho"><i class="fas fa-shopping-cart"></i></a>
 			<span class="badge bg-warning"></span>
 		</div>
