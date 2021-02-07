3feb4567d0b9a851b84c1560bbddf67ede29c499
Omnivoro <omnivoro@gmail.com>
2021-02-07 08:51:06 +0000

    Adding css style to the links in header.php
    Creating two css classes for the links in the header.php file.

--- core/views/layouts/header.php
+++ core/views/layouts/header.php
@@ -11,15 +11,15 @@
 			</a>
 		</div>			
 		<div class="col-6 text-end p-3">		
-			<a href="?a=inicio">Início</a>
-			<a href="?a=loja">Loja</a>
+			<a href="?a=inicio" class="nav-item">Início</a>
+			<a href="?a=loja" class="nav-item">Loja</a>
 			<!--Verifca se existe cliente na sessão -->
 			<?php if(Store::clienteLogado()):?>
-				<a href="">A minha conta</a>
-				<a href="">Logout</a>
+				<a href="" class="nav-item">A minha conta</a>
+				<a href="" class="nav-item">Logout</a>
 			<?php else:?>
-				<a href="">Login</a>
-				<a href="">Criar conta</a>
+				<a href="" class="nav-item">Login</a>
+				<a href=""class="nav-item">Criar conta</a>
 			<?php endif;?>
 			<a href="?a=carrinho"><i class="fas fa-shopping-cart"></i></a>
 			<span class="badge bg-warning"></span>
