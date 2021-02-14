ed83795b12909fb88620edceb035ba771da1182f
Omnivoro <omnivoro@gmail.com>
2021-02-14 10:48:27 +0000

    Creating the registar_cliente() method
    The resistar cliente method was created in the Clientes class and this
    method returns the $purl variable value.

--- core/controllers/Main.php
+++ core/controllers/Main.php
@@ -86,45 +86,15 @@ class Main{
 			$this->novo_cliente();
 			return;
 		}
-
 		
-		// cliente pronto para ser inserido na base de dados
-        $purl = Store::criarHash();
-		
-		$parametros = [
-            ':email' => strtolower(trim($_POST['text_email'])),
-            ':senha' => password_hash(trim($_POST['text_senha_1']), PASSWORD_DEFAULT),
-            ':nome_completo' => trim($_POST['text_nome_completo']),
-            ':morada' => trim($_POST['text_morada']),
-            ':cidade' => trim($_POST['text_cidade']),
-            ':telefone' => trim($_POST['text_telefone']),
-            ':purl' => $purl,
-            ':ativo' => 0
-        ];
-		
-        $bd->insert("
-            INSERT INTO clientes VALUES(
-                0,
-                :email,
-                :senha,
-                :nome_completo,
-                :morada,
-                :cidade,
-                :telefone,
-                :purl,
-                :ativo,
-                NOW(),
-                NOW(),
-                NULL
-            )
-        ", $parametros);
-
+		// inserir cliente na base de dados e devolver o purl
+		$purl = $cliente -> registar_cliente();
+			
 		// criar o link purl para enviar por email
         $link_purl = "https://phpstore-svvtc.run-eu-central1.goorm.io/phpstore/public/?a=confirmar_email&purl=$purl";
 		
 	}
 		
-	
 	// ===========================================================
     public function carrinho(){
         
--- core/models/Clientes.php
+++ core/models/Clientes.php
@@ -3,6 +3,7 @@
 namespace core\models;
 
 use core\classes\Database;
+use core\classes\Store;
 
 
 class Clientes{
@@ -28,6 +29,46 @@ class Clientes{
 			return false;
 		}
 	}
+	
+	// ============================================================
+	public function  registar_cliente(){
+		
+	$bd = new Database();	
+	
+    $purl = Store::criarHash();
+		
+	$parametros = [
+        ':email' => strtolower(trim($_POST['text_email'])),
+        ':senha' => password_hash(trim($_POST['text_senha_1']), PASSWORD_DEFAULT),
+        ':nome_completo' => trim($_POST['text_nome_completo']),
+        ':morada' => trim($_POST['text_morada']),
+        ':cidade' => trim($_POST['text_cidade']),
+        ':telefone' => trim($_POST['text_telefone']),
+        ':purl' => $purl,
+        ':ativo' => 0
+   ];
+		
+    $bd->insert("
+        INSERT INTO clientes VALUES(
+               0,
+            :email,
+            :senha,
+			:nome_completo,
+            :morada,
+            :cidade,
+            :telefone,
+        	:purl,
+            :ativo,
+            NOW(),
+            NOW(),
+			NULL
+        )
+    ", $parametros);
+		
+	return $purl;
+		
+	}
+		
 }
 
 ?>
