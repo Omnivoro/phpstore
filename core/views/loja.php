b05281e6a5a49ec1fad4ab3fa9dc871d29a68ad9
Omnivoro <omnivoro@gmail.com>
2021-02-20 12:20:03 +0000

    fix: Modified anchors category to make them dynamic

--- core/views/loja.php
+++ core/views/loja.php
@@ -1,11 +1,10 @@
-<?php //$produto = $produtos[0]; ?>
-
 <div class="container espaco-fundo">
 	<div class="row">
 		<div class="col-12 text-center my-4">
 			<a href="?a=loja&c=todos" class="btn btn-primary">Todos</a>
-			<a href="?a=loja&c=homem" class="btn btn-primary">Homem</a>
-			<a href="?a=loja&c=mulher" class="btn btn-primary">Mulher</a>
+			<?php foreach($categorias as $categoria): ?>
+				<a href="?a=loja&c=<?= $categoria ?>" class="btn btn-primary"><?= ucfirst($categoria) ?></a>
+			<?php endforeach; ?>
 		</div>
 	</div>
 	
@@ -25,19 +24,3 @@
 		<?php endforeach; ?>
 	</div>
 </div>
-
-<!--
-stdClass Object
-(
-    [id_produto] => 1
-    [categoria] => homem
-    [nome_produto] => Tshirt Vermelha
-    [descricao] => Ab laborum, commodi aspernatur, quas distinctio cum quae omnis autem ea, odit sint quisquam similique! Labore aliquam amet veniam ad fugiat optio.
-    [imagem] => tshirt_vermelha.png
-    [preco] => 45.70
-    [stock] => 100
-    [visivel] => 1
-    [created_at] => 2021-02-06 19:45:18
-    [updated_at] => 2021-02-06 19:45:25
-    [deleted_at] => 
-)-->
