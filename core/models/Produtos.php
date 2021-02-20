afe4fda65a1a5bbce188745c927618cbdfde3660
Omnivoro <omnivoro@gmail.com>
2021-02-20 12:10:17 +0000

    feat: Create the lista_categorias() method in the Produto model
    Use this method inside the list_produtos-disponiveis() method to change
    $sql varible according to $categoria variable received as parameter.

--- core/models/Produtos.php
+++ core/models/Produtos.php
@@ -15,15 +15,31 @@ class Produtos{
 		
 		$sql = "SELECT * FROM produtos WHERE visivel = 1";
 		
-		if($categoria == 'homem' || $categoria == 'mulher'){
+		// buscar lista de categorias
+		$categorias = $this->lista_categorias();
+		
+		if(in_array($categoria, $categorias))
 			$sql .= " AND categoria = '$categoria'";
-		}
 		
 		// busca toda a informação dos produtos disponíveis na base de dados
 		$produtos = $bd->select($sql);
 		
 		return $produtos;
 	}
+	
+	// ============================================================
+	public function lista_categorias(){
+		
+		$bd = new Database();
+		
+		$resultados = $bd->select("SELECT DISTINCT(categoria) FROM produtos");
+		$categorias = [];
+		foreach($resultados as $resultado)
+			array_push($categorias, $resultado->categoria);
+
+		return $categorias;
+	}
+	
 }
 
 ?>
