e039bd59ecea402b69038310a06023ccf0ee67b3
Omnivoro <omnivoro@gmail.com>
2021-02-21 10:18:17 +0000

    fix: Add a if condition to filter products without stock

--- core/views/loja.php
+++ core/views/loja.php
@@ -10,17 +10,23 @@
 	
 	<!-- produtos -->
 	<div class="row">
-		<?php foreach($produtos as $produto): ?>
-			<div class="col-sm-4 col-6 p-2">
-				<div class="text-center p-3 box-produto">
-					<img src="assets/images/produtos/<?= $produto->imagem ?>" class="img-fluid" >
-					<h3><?= $produto->nome_produto ?></h3>
-					<h2><?= $produto->preco ?></h2>
-					<div>
-						<button>Addicionar ao carrinho</button>
+		<?php if(count($produtos) == 0): ?>
+			<div class="text-center my-5">
+				<h3>Não existem produtos disponíveis.</h3>
+			</div>
+		<?php else: ?>
+			<?php foreach($produtos as $produto): ?>
+				<div class="col-sm-4 col-6 p-2">
+					<div class="text-center p-3 box-produto">
+						<img src="assets/images/produtos/<?= $produto->imagem ?>" class="img-fluid" >
+						<h3><?= $produto->nome_produto ?></h3>
+						<h2><?= $produto->preco ?></h2>
+						<div>
+							<button>Addicionar ao carrinho</button>
+						</div>
 					</div>
 				</div>
-			</div>
-		<?php endforeach; ?>
+			<?php endforeach; ?>
+		<?php endif;?>
 	</div>
 </div>
