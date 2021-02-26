6adb34c7af7638b05e9df385791f287d6e23c2f3
Omnivoro <omnivoro@gmail.com>
2021-02-26 09:48:16 +0000

    fix: Modify the carrinho layout to validate $_SESSION['carrinho']

--- core/views/carrinho.php
+++ core/views/carrinho.php
@@ -1,13 +1,21 @@
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-12">
-			<h3>
-				Carrinho!
-			</h3>
-			<a href="?a=limpar_carrinho" class="btn btn-sm btn-primary">Limpar carrinho</a>
-			<pre>
-				<?php print_r($_SESSION); ?>
-			</pre>
-		</div>
-	</div>
+<h3>Carrinho!</h3>
+<a href="?a=limpar_carrinho" class="btn btn-sm btn-primary">Limpar carrinho</a>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+        <?php if($carrinho == null):?>
+            <p>Carrinho vazio</p>
+            <p><a href="?a=loja" class="btn btn-primary">Loja</a></p>
+        <?php else: ?>
+
+            <p>Carrinho...</p>
+
+            
+        <?php endif; ?>
+
+        </div>
+    </div>
 </div>
