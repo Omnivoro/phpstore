6591aba8af318c06869b4ce97afbad5294dab187
Omnivoro <omnivoro@gmail.com>
2021-03-01 19:50:49 +0000

    feat: Create table to show products in the carrinho layout

--- core/views/carrinho.php
+++ core/views/carrinho.php
@@ -1,3 +1,4 @@
+<?php $total_quantidade = 0 ?>
 <h3>Carrinho!</h3>
 <a href="?a=limpar_carrinho" class="btn btn-sm btn-primary">Limpar carrinho</a>
 
@@ -10,12 +11,43 @@
             <p>Carrinho vazio</p>
             <p><a href="?a=loja" class="btn btn-primary">Loja</a></p>
         <?php else: ?>
-
-            <p>Carrinho...</p>
-
-            
+			<div style="margin-button: 80px;">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>imgens</th>
+							<th>Produto</th>
+							<th>Quantidade</th>
+							<th class="text-end">Valor sub-total</th>
+							<th>ações</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach($carrinho as $produto):?>
+							<!-- lista de produtos -->
+							<tr>
+								<td><img src="assets/images/produtos/<?= $produto['imagem']?>" class="img-fluid" width="50px"/></td>
+								<td><?= $produto['titulo'] ?></td>
+								<td><?= $produto['quantidade'];?></td>
+								<td class="text-end"><?= $produto['preco'] ?></td>
+								<td><button class="btn btn-danger">
+									<i class="fas fa-times"></i>
+								</button></td>
+							</tr>
+							<?php $total_quantidade += $produto['quantidade']; ?>
+						<?php endforeach;?>
+							<!-- total -->
+							<tr>	
+								<td></td>
+								<td></td>
+								<td><?= $total_quantidade ?></td>
+								<td class="text-end"><?= $total?></td>
+								<td>Total</td>
+							</tr>
+					</tbody>
+				</table>
+			</div>
         <?php endif; ?>
-
         </div>
     </div>
 </div>
